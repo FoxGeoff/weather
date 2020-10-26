2b980de4b73516d6005653c9889c499834d0c201
Geoff Fox <fox.geoffrey@gmail.com>
2020-10-26 14:35:13 -0700

    using fs-extra (node readFiles3-fs-extra)

---
+++ async/readFiles3-fs-extra.js
@@ -0,0 +1,19 @@
+const fs = require("fs-extra");
+
+// define an anonymous function
+const readAllFiles = async () => {
+  let myText = "";
+  try {
+    myText = await fs.readFile("1.txt");
+    myText += await fs.readFile("2.txt");
+    myText += await fs.readFile("3.txt");
+    myText += await fs.readFile("4.txt");
+    myText += await fs.readFile("5.txt");
+    console.log(myText);
+  } catch (err) {
+    console.log(`Error! ${err.message}`);
+  }
+};
+
+// call the function
+readAllFiles();
