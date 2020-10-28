a2a9631c02fd73ac2f426c83637b64eccb58df74
Geoff Fox <fox.geoffrey@gmail.com>
2020-10-28 11:05:48 -0700

    A first web application. Part 2

--- numbers/index.js
+++ numbers/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs-extra");
+const qs = require("querystring")
 
 // Create http Server object
 http
@@ -17,8 +18,24 @@ http
 
       // Data complete
       req.on("end", () => {
+
+        // Parse the body into usable POST data
+        const query = qs.parse(body);
+        
+        // Create some content to respond with
+        const num1 = parseInt(query.num1);
+        const num2 = parseInt(query.num2);
+        const results = {
+          num1: num1,
+          num2: num2,
+          add: num1 + num2,
+          sub: num1 - num2,
+          div: num1 / num2,
+          mul: num1 * num2
+        };
+
         // Send the results page
-        serveFile(res, "results.html");
+        serveFile(res, "results.html", results);
       });
     }
   })
@@ -27,10 +44,27 @@ http
   });
 
 // Serve a static file
-async function serveFile(res, filename) {
+async function serveFile(res, filename, keyPairs) {
   // Get the file
   console.log(`Serving ${filename}`);
   let content = await fs.readFile(filename, "utf-8");
+
+  // Fill in the blanks (tokens {})
+  if(typeof keyPairs != 'undefined') {
+
+    // Get list of keys
+    const keys = Object.keys(keyPairs);
+
+    //for each key
+    for (key in keys) {
+
+      // Get the key name
+      const keyName = keys[key];
+
+      // Replace all instances of the key name with the key value
+      content = content.replace(new RegExp(`{${keyName}}`, "g"), keyPairs[keyName]);
+    }
+  }
 
   // Write out the content
   res.writeHead(200, { "Content-Type": "text/html" });
