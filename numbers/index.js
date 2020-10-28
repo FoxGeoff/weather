7a71843da900945e1b2a275c5782bc6cc7cf0ee2
Geoff Fox <fox.geoffrey@gmail.com>
2020-10-28 07:13:10 -0700

    Now we will check the header verb (GET vs POST)

--- numbers/index.js
+++ numbers/index.js
@@ -4,7 +4,23 @@ const fs = require("fs-extra");
 // Create http Server object
 http
   .createServer(async (req, res) => {
-    serveFile(res, "form.html");
+    if (req.method == "GET") {
+      serveFile(res, "form.html");
+    } else if (req.method == "POST") {
+      // Get the submited data
+      let body = "";
+
+      // Data has arrived
+      req.on("data", (data) => {
+        body += data;
+      });
+
+      // Data complete
+      req.on("end", () => {
+        // Send the results page
+        serveFile(res, "results.html");
+      });
+    }
   })
   .listen(8080, (err) => {
     console.log("Server running at http://127.0.0.1:8080/");
